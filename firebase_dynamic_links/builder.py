4c1072861c19cad860804b62f0b19a430a385f0c
begum-akbay <begum@mosaic.ie>
2020-06-08 15:26:40 +0100

    Update: Ability to support full domain is implemented

--- firebase_dynamic_links/builder.py
+++ firebase_dynamic_links/builder.py
@@ -3,28 +3,31 @@ from urllib.parse import urlencode
 from .client import FirebaseClient
 
 
-def generate_short_link(client, app_code, query_params):
-    long_dynamic_link = generate_long_link(app_code=app_code, query_params=query_params)
+def generate_short_link(client, app_code, query_params, is_partial_link):
+    long_dynamic_link = generate_long_link(app_code=app_code, is_partial_link=is_partial_link, query_params=query_params)
 
     return client.shorten_link(long_link=long_dynamic_link)
 
 
-def generate_long_link(app_code, query_params):
+def generate_long_link(app_code, query_params, is_partial_link):
     query_string = urlencode(query_params)
-    link = 'https://{app_code}.page.link/?{query_string}'.format(app_code=app_code, query_string=query_string)
+    domain = 'https://{app_code}.page.link'.format(app_code=app_code) if is_partial_link else app_code
+    
+    link = '{domain}/?{query_string}'.format(domain=domain, query_string=query_string)
 
     return link
+
 
 
 class DynamicLinkBuilder:
     def __init__(self, client):
         self.client = client
 
-    def generate_long_link(self, app_code, **kwargs):
-        return generate_long_link(app_code=app_code, query_params=kwargs)
+    def generate_long_link(self, app_code, is_partial_link=False, **kwargs):
+        return generate_long_link(app_code=app_code, query_params=kwargs, is_partial_link=is_partial_link)
 
-    def generate_short_link(self, app_code, **kwargs):
-        return generate_short_link(client=self.client, app_code=app_code, query_params=kwargs)
+    def generate_short_link(self, app_code, is_partial_link=False, **kwargs):
+        return generate_short_link(client=self.client, app_code=app_code, query_params=kwargs, is_partial_link=is_partial_link)
 
 
 def dynamic_link_builder(api_key):
